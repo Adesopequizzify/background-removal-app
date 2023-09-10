53fcf8628d7c842a44f95eaad5ae9c09af31f9bb
adesope@quizzify.com.ng <adesope@quizzify.com.ng>
2023-09-10 17:06:57 +0100

    update

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Upload and Preview</title>
+    <title>Upload image |TechQue-Tools</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+     <link rel="stylesheet" type="text/css" href="/assets/css/bg.css">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css">
 </head>
 <body>
@@ -18,13 +19,14 @@
         <p>Unleash the power to remove backgrounds.</p>
     </div>
     <div class="header-image">
-        <img src="background-removal-icon.png" alt="Background Removal Icon">
+        <img src="/assets/images/down.png" alt="Background Removal Icon">
     </div>
 </header>
 <div class="container">
         <form id="imageUploadForm" class="card">
             <h2>Upload Image</h2>
             <label for="imageInput" class="custom-file-icon">
+              
                 <input type="file" id="imageInput" accept="image/*">
                 <span><i class="fa fa-cloud-upload"></i> Upload Image</span>
             </label>
@@ -40,6 +42,20 @@
             <a id="downloadButton" class="download-button" download>Download</a>
         </div>
     </div>
+        <footer class=" ad bg-dark text-white py-3">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <p>&copy; 2023 TechQue. All Rights Reserved</p>
+              </div>
+              <div class="col-md-6 text-center">
+                <a href="https://www.facebook.com/Official.Adesope" class="text-white mx-3"><i class="fab fa-facebook"></i></a>
+                <a href="https://github.com/adesopequizzify" class="text-white mx-3"><i class="fab fa-github"></i></a>
+                <a href="https://twitter.com/adesopemuiz3" class="text-white mx-3"><i class="fab fa-twitter"></i></a>
+              </div>
+            </div>
+          </div>
+        </footer>
     <script src="/assets/js/main.js"></script>
 </body>
 </html>
